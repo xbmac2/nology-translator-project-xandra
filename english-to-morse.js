6cad12422bd1fe07d409fd0b19b7f07838cb397b
Xandra <xandra.macatangay@gmail.com>
2023-12-31 16:48:25 +1100

    eng to morse with btn

---
+++ english-to-morse.js
@@ -0,0 +1,25 @@
+const encoder = (stringToEncode) => {
+  //creates array of uppercase characters and / for spaces
+  const capitalisedStr = stringToEncode.toUpperCase().replaceAll(" ", "/");
+  const messageArr = Array.from(capitalisedStr);
+  const cleanedMessageArr = messageArr.filter((char) => /[0-9A-Z\/]/.test(char));
+  //iterates over array to make characters into morse strings
+  let morseArr = [];
+  for (let i = 0; i < cleanedMessageArr.length; i++) {
+    if (/[A-Z]/.test(cleanedMessageArr[i])) {
+      morseArr.push(cypherAlphabet[cleanedMessageArr[i]]);
+    } else if (cleanedMessageArr[i] === "/") {
+      morseArr.push("/")
+    } else if (/[0-9]/.test(cleanedMessageArr[i])) {
+      morseArr.push(cypherNumerals[cleanedMessageArr[i]]);
+    };
+  };
+  const morseStr = morseArr.join(" ");
+  //console.log(morseStr);
+  document.querySelector("#morseOutput").innerText = morseStr;
+};
+
+//clicking button
+document.querySelector("#translateBtn").addEventListener("click", () => {
+  encoder(document.querySelector("#englishInput").value)
+});
