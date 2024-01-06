7bb5e5daff9c724dad1f2f3719821c53a2ed8f39
Xandra <xandra.macatangay@gmail.com>
2024-01-06 11:40:55 +1100

    uses two buttons

--- dom-functions.js
+++ dom-functions.js
@@ -34,10 +34,27 @@ document.querySelector("#decoderBtn").addEventListener("click", () => {
   }
 });
 
-//adding input to textarea translates eng to morse
-//this was working
+//adding input to textarea translates either way
+//the below works until user tries to mix inputs
+
 // document.querySelector("textarea").addEventListener("input", () => {
-//   //DISABLEFOR NOW AND MAKE BUTTON ONLY TO TEST DECODERdocument.querySelector("#translatedOutput").innerText = encoder(document.querySelector("#userInput").value);
+
+//   const userInputArr = document.querySelector("textarea").value.split("");
+
+//   function checkMorseChars(char) {
+//     return /[\s\.\-\/]/.test(char);
+//   }
+
+//   if (userInputArr.every(checkMorseChars)) {
+//     //run decoder aka assume morse
+//     console.log("working there");
+//     console.log(userInputArr);
+//     document.querySelector("#translatedOutput").innerText = decoder(document.querySelector("#userInput").value);
+//   } else {
+//     //run encoder aka assume english
+//     console.log("working here");
+//     document.querySelector("#translatedOutput").innerText = encoder(document.querySelector("#userInput").value);
+//   }
 
 //   //resize output field to fit overflowing paragraph morse
 //   const outputHeight = document.querySelector("p").scrollHeight;
@@ -46,32 +63,3 @@ document.querySelector("#decoderBtn").addEventListener("click", () => {
 //     document.querySelector(".output-field").style.height = `${outputHeight + 30}px`;
 //   }
 // });
-
-//adding input to textarea translates either way
-//the below works until user tries to mix inputs
-document.querySelector("textarea").addEventListener("input", () => {
-
-  const userInputArr = document.querySelector("textarea").value.split("");
-
-  function checkMorseChars(char) {
-    return /[\s\.\-\/]/.test(char);
-  }
-
-  if (userInputArr.every(checkMorseChars)) {
-    //run decoder aka assume morse
-    console.log("working there");
-    console.log(userInputArr);
-    document.querySelector("#translatedOutput").innerText = decoder(document.querySelector("#userInput").value);
-  } else {
-    //run encoder aka assume english
-    console.log("working here");
-    document.querySelector("#translatedOutput").innerText = encoder(document.querySelector("#userInput").value);
-  }
-
-  //resize output field to fit overflowing paragraph morse
-  const outputHeight = document.querySelector("p").scrollHeight;
-  if (outputHeight > document.querySelector("textarea").scrollHeight) {
-    document.querySelector(".translations-container").style.height = `${outputHeight + 30}px`;
-    document.querySelector(".output-field").style.height = `${outputHeight + 30}px`;
-  }
-});
